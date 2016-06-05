3967d3304c904a2d05acb21935221ec801f66f30
Snowy <snowyman@inbox.ru>
2016-06-05 19:55:00 +0100

    Чуть-чуть скриптов генератора и потребителей

---
+++ Assets/Game/Scripts/Consumers.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Consumers : MonoBehaviour {
+
+    public bool isEnabled = false;
+    public bool isImportant = false;
+    public byte Priority = 0;
+
+    public int needEnergy;
+    public int nowEnergy;
+
+    public void SetActive()
+    {
+        nowEnergy = needEnergy;
+        isEnabled = true;
+    }
+
+}
--- Assets/Game/Scripts/Generator.cs
+++ Assets/Game/Scripts/Generator.cs
@@ -2,14 +2,60 @@
 using System.Collections;
 
 public class Generator : MonoBehaviour {
+    public int Energy;
+    private int forAllEnergy;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public System.Collections.Generic.List<GameObject> Consumers = new System.Collections.Generic.List<GameObject>();
+
+    void Update()
+    {
+        
+        if (checkEnergyForAll())
+        {
+            foreach (GameObject consumer in Consumers) {
+                consumer.GetComponent<Consumers>().SetActive();
+            }
+        }
+        else
+        {
+            if (checkEnergyForImportant())
+            {
+                foreach (GameObject consumer in Consumers)
+                {
+                    Consumers selectedConsumer = consumer.GetComponent<Consumers>();
+                    if (selectedConsumer.isImportant)
+                    {
+                        selectedConsumer.SetActive();
+                    }
+                }
+            }
+        }
+    }
+    private bool checkEnergyForAll()
+    {
+        foreach (GameObject consumer in Consumers)
+        {
+            forAllEnergy += consumer.GetComponent<Consumers>().needEnergy;
+        }
+        bool returnedValue = (forAllEnergy <= Energy) ? true : false;
+        forAllEnergy = 0;
+        return returnedValue;
+    }
+    private bool checkEnergyForImportant()
+    {
+        foreach (GameObject consumer in Consumers)
+        {
+            Consumers selectedConsumer = consumer.GetComponent<Consumers>();
+            if (selectedConsumer.isImportant)
+            {
+                forAllEnergy += selectedConsumer.needEnergy;
+            }
+        }
+        bool returnedValue = (forAllEnergy <= Energy) ? true : false;
+        forAllEnergy = 0;
+        return returnedValue;
+    }
+    void SortForPriority()
+    {
+    }
 }
